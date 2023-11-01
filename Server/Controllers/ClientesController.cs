cd21a9962f0a53f06e9152489a3e55537532f5de
Randy <randyfabianbelliard191407@gmail.com>
2023-11-01 19:05:54 -0400

    Creando troladores

---
+++ Server/Controllers/ClientesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistrosWeb.Server.DAL;
+using RegistrosWeb.Shared.Models;
+
+namespace RegistrosWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public ClientesController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clientes>>> GetClientes()
+        {
+            if (_context.Clientes == null)
+            {
+                return NotFound();
+            }
+            return await _context.Clientes.ToListAsync();
+        }
+
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clientes>> GetClientes(int id)
+        {
+            if (_context.Clientes == null)
+            {
+                return NotFound();
+            }
+            var clientes = await _context.Clientes.FindAsync(id);
+
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return clientes;
+        }
+
+        // POST: api/Clientes
+        [HttpPost]
+        public async Task<ActionResult<Clientes>> PostClientes(Clientes clientes)
+        {
+            if (!ClientesExists(clientes.ClienteId))
+                _context.Clientes.Add(clientes);
+            else
+                _context.Clientes.Update(clientes);
+
+            await _context.SaveChangesAsync();
+            return Ok(clientes);
+        }
+
+        // DELETE: api/Clientes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClientes(int id)
+        {
+            if (_context.Clientes == null)
+            {
+                return NotFound();
+            }
+            var clientes = await _context.Clientes.FindAsync(id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(clientes);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientesExists(int id)
+        {
+            return (_context.Clientes?.Any(e => e.ClienteId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Server/Controllers/TicketsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistrosWeb.Server.DAL;
+using RegistrosWeb.Shared.Models;
+
+namespace RegistrosWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public TicketsController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tickets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tickets>>> GetTickets()
+        {
+            if (_context.Tickets == null)
+            {
+                return NotFound();
+            }
+            return await _context.Tickets.ToListAsync();
+        }
+
+        // GET: api/Tickets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tickets>> GetTickets(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return NotFound();
+            }
+            var tickets = await _context.Tickets.FindAsync(id);
+
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            return tickets;
+        }
+
+        // POST: api/Tickets
+        [HttpPost]
+        public async Task<ActionResult<Tickets>> PostTickets(Tickets tickets)
+        {
+            if (!TicketsExists(tickets.TicketsId))
+                _context.Tickets.Add(tickets);
+            else
+                _context.Tickets.Update(tickets);
+
+            await _context.SaveChangesAsync();
+            return Ok(tickets);
+        }
+
+        // DELETE: api/Tickets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTickets(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return NotFound();
+            }
+            var tickets = await _context.Tickets.FindAsync(id);
+            if (tickets == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tickets.Remove(tickets);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketsExists(int id)
+        {
+            return (_context.Tickets?.Any(e => e.TicketsId == id)).GetValueOrDefault();
+        }
+    }
+}
+
